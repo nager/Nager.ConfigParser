d21b8573264d452b514b88d2617890d702369c21
petber <bernhard.petautschnig@alturos.com>
2020-01-20 14:56:49 +0100

    added new double parser units

---
+++ src/Nager.ConfigParser/ParserUnit/DoubleArrayParserUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Nager.ConfigParser.ParserUnit
+{
+    internal class DoubleArrayParserUnit : BaseParserUnit
+    {
+        public override Type ParserUnitType => typeof(double[]);
+
+        public override object Deserialize(string value)
+        {
+            var parts = value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return parts.Select(o =>
+            {
+                var success = double.TryParse(o, out double temp);
+                return new { success, temp };
+            })
+            .Where(o => o.success)
+            .Select(o => o.temp).ToArray();
+        }
+
+        public override string Serialize(object value)
+        {
+            if (value is double[] items)
+            {
+                return string.Join(",", items);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Nager.ConfigParser/ParserUnit/DoubleParserUnit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nager.ConfigParser.ParserUnit
+{
+    internal class DoubleParserUnit : BaseParserUnit
+    {
+        public override Type ParserUnitType => typeof(double);
+
+        public override object Deserialize(string value)
+        {
+            if (!double.TryParse(value, out var temp))
+            {
+                return temp;
+            }
+
+            return 0;
+        }
+
+        public override string Serialize(object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return $"{value}";
+        }
+    }
+}
