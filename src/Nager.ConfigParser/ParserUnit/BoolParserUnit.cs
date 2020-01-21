7703d61f9b64e74ab1c0b195a37e28d8f6d7bf19
Tino Hager <tino.hager@nager.at>
2020-01-21 20:59:14 +0100

    Add bool parser unit

---
+++ src/Nager.ConfigParser/ParserUnit/BoolParserUnit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nager.ConfigParser.ParserUnit
+{
+    internal class BoolParserUnit : BaseParserUnit
+    {
+        public override Type ParserUnitType => typeof(bool);
+
+        public override object Deserialize(string value)
+        {
+            bool.TryParse(value, out var result);
+            return result;
+        }
+
+        public override string Serialize(object value)
+        {
+            return value.ToString();
+        }
+    }
+}
